8b0d56780443ef9e51bec1329f1175caa250a35f
liquidcronos <jan-baumgaertner@web.de>
2019-08-02 14:07:45 +0200

    fixed behavior indexing

--- scripts/fusion.py
+++ scripts/fusion.py
@@ -9,14 +9,15 @@ class Fusion:
 	def __init__(self):
                 # array containes cat,blue,orange,cheese,collision
                 self.behaviors = np.zeros((5,2))
-                parameters     =  np.array([[-0.1,-1,-1],[0.3,0,1],[0,0,1],[0.8,0,1],[-0.5,-1,-1]])
-                for i in range(1,5):
-                    rospy.Subscriber(sys.argv[i], Point, self.generate_behavior,(i,parameters[i,0],parameters[i,1],parameters[i,2]))
+                parameters     =  np.array([[-0.5,-1,-1],[0.3,0,1],[0,0,1],[0.8,0,1],[-0.5,-1,-1]])
+                for i in range(5):
+                    rospy.Subscriber(sys.argv[i+1], Point, self.generate_behavior,(i,parameters[i,0],parameters[i,1],parameters[i,2]))
 		self.pub                     = rospy.Publisher('/RosAria/cmd_vel', Twist, queue_size=1)
 		while not rospy.is_shutdown():
 			self.fusion()
 
         def generate_behavior(self,msg,args):
+                print(args[0])
                 if msg.x != 0 and msg.y != 0 :
                     self.behaviors[args[0]] = np.array([1,args[1]*msg.x**args[2]*msg.y**args[3]])
                 else:
@@ -26,8 +27,9 @@ class Fusion:
 		#print('collision', self.collision, 'cheese', self.cheese)
 		cmd_vel = Twist()
 		cmd_vel.linear.x = 1
-                cmd_vel.angular.z = prevail_gate(or_gate(invoke_gate(self.behaviors[0,1],self.behaviors[2,1]),self.behaviors[3,1]),self.behaviors[4,1])
-                print(cmd_vel)
+                cmd_vel.angular.z = prevail_gate(or_gate(invoke_gate(self.behaviors[2,1],self.behaviors[0,1]),self.behaviors[3,1]),self.behaviors[4,1])
+                #cmd_vel.angular.z = self.behaviors[1,1]
+                print(self.behaviors)
 		self.pub.publish(cmd_vel)
 
 	
