e38cce51d63e24d22151e97e9bf682072a63b760
Jan Baumgärtner <22688144+liquidcronos@users.noreply.github.com>
2022-02-25 10:04:42 +0100

    cleaned up code

--- scripts/fusion.py
+++ scripts/fusion.py
@@ -5,53 +5,58 @@ import numpy as np
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Point
 
+
 class Fusion:
-	def __init__(self):
-                # array containes cat,blue,orange,cheese,collision
-                self.behaviors = np.zeros(5)
-                parameters     =  np.array([[-0.5,-1,1,-1],[0.3,0,1,1],[0,0,1,1],[0.8,0,1,1],[-0.5,-1,1,-1]])
-                for i in range(5):
-                    rospy.Subscriber(sys.argv[i+1], Point, self.generate_behavior,(i,parameters[i,0],parameters[i,1],parameters[i,2]))
-		self.pub = rospy.Publisher('/RosAria/cmd_vel', Twist, queue_size=1)
-		while not rospy.is_shutdown():
-			self.fusion()
+    def __init__(self):
+        # array containes cat,blue,orange,cheese,collision
+        self.behaviors = np.zeros(5)
+        parameters = np.array([[-0.5, -1, 1, -1], [0.3, 0, 1, 1],
+                              [0, 0, 1, 1], [0.8, 0, 1, 1], [-0.5, -1, 1, -1]])
+        for i in range(5):
+            rospy.Subscriber(sys.argv[i+1], Point, self.generate_behavior,
+                             (i, parameters[i, 0], parameters[i, 1], parameters[i, 2], parameters[i, 3]))
+        self.pub = rospy.Publisher('/RosAria/cmd_vel', Twist, queue_size=1)
+        while not rospy.is_shutdown():
+            self.fusion()
 
-        def generate_behavior(self,msg,args):
-                if msg.x != 0 and msg.y != 0 :
-                    self.behaviors[args[0]] =  AND(sigmoid(msg.x**args[2],args[1],0),sigmoid(msg.y**args[4],args[3]))
-                else:
-                    self.behaviors[args[0]] = 0
-	def fusion(self):
-		#print('collision', self.collision, 'cheese', self.cheese)
-		cmd_vel = Twist()
-		cmd_vel.linear.x = 1
-                cmd_vel.angular.z = PREVAIL(OR(INVOKE(self.behaviors[2],self.behaviors[0]),self.behaviors[3]),self.behaviors[4])
-                #cmd_vel.angular.z = self.behaviors[1,1]
-                print(self.behaviors)
-		self.pub.publish(cmd_vel)
+    def generate_behavior(self, msg, args):
+        if msg.x != 0 and msg.y != 0:
+            self.behaviors[args[0]] = AND(
+                sigmoid(msg.x**args[2], args[1], 0), sigmoid(msg.y**args[4], args[3]))
+        else:
+            self.behaviors[args[0]] = 0
+
+    def fusion(self):
+        cmd_vel = Twist()
+        cmd_vel.linear.x = 1
+        cmd_vel.angular.z = PREVAIL(OR(INVOKE(
+            self.behaviors[2], self.behaviors[0]), self.behaviors[3]), self.behaviors[4])
+        self.pub.publish(cmd_vel)
 
 
+def sigmoid(x, steepness, midpoint):
+    return 2/(1+np.exp(-steepness*(x-midpoint)))-1
 
 
-def sigmoid(x,steepness,midpoint):
-        return 2/(1+np.exp(-steepness*(x-midpoint)))-1
+def OR(x, y):
+    return x+y-x*y*np.tanh(x+y)/np.tanh(2)
 
-def OR(x,y):
-    return  x+y-x*y*np.tanh(x+y)/np.tanh(2)
 
-def AND(x,y):
+def AND(x, y):
     return x*y*np.tanh(x+y)/np.tanh(2)
-    
-def INVOKE(x,y):
-    return AND(x,OR(x,y))
-    
-    
-def PREVAIL(x,y):
-    return OR(x,OR(x, y))
+
+
+def INVOKE(x, y):
+    return AND(x, OR(x, y))
+
+
+def PREVAIL(x, y):
+    return OR(x, OR(x, y))
+
 
 if __name__ == '__main__':
-	try:
-		rospy.init_node("fusion")
-		fus = Fusion()	
-	except rospy.ROSInterruptException:
-		rospy.loginfo("---------- FUSION-ERROR! ---------")
+    try:
+        rospy.init_node("fusion")
+        fus = Fusion()
+    except rospy.ROSInterruptException:
+        rospy.loginfo("---------- FUSION-ERROR! ---------")
--- scripts/sonar.py
+++ scripts/sonar.py
@@ -5,36 +5,44 @@ from sensor_msgs.msg import PointCloud
 from geometry_msgs.msg import Point
 
 max_distance = 1
-weight       = np.array([  0.5,   0.7,   1.0,   1.0,  1.0,  1.0,  0.7,  0.5])
-sonar_angles = np.array([-90.0, -50.0, -30.0, -10.0, 10.0, 30.0, 50.0, 90.0])/ 360.0 * 2 * np.pi
+weight = np.array([0.5,   0.7,   1.0,   1.0,  1.0,  1.0,  0.7,  0.5])
+sonar_angles = np.array(
+    [-90.0, -50.0, -30.0, -10.0, 10.0, 30.0, 50.0, 90.0]) / 360.0 * 2 * np.pi
+
+
 class Sonar:
     def __init__(self):
         self.hideout = np.zeros(2)
-        obst_sub  = rospy.Subscriber("/RosAria/sonar",PointCloud,self.sonar_callback)
-        blue_sub  = rospy.Subscriber("blue_position",Point,self.hideout_callback,0)
-        orang_sub = rospy.Subscriber("orange_position",Point,self.hideout_callback,1)
+        rospy.Subscriber("/RosAria/sonar", PointCloud, self.sonar_callback)
+        rospy.Subscriber("blue_position", Point, self.hideout_callback, 0)
+        rospy.Subscriber("orange_position", Point, self.hideout_callback, 1)
         while not rospy.is_shutdown():
             print("still working")
-    def hideout_callback(self,data,i):
+
+    def hideout_callback(self, data, i):
         if data.x != 0:
             self.hideout[i] = data.x
         else:
             self.hideout[i] = 100000
-    def sonar_callback(self,data):
-        pub       = rospy.Publisher("obstacle_position",Point,queue_size=1)
+
+    def sonar_callback(self, data):
+        pub = rospy.Publisher("obstacle_position", Point, queue_size=1)
         sonar_points = data.points
         sonar_ranges = np.zeros(len(sonar_angles))
         for i in range(0, len(sonar_angles)):
-            sonar_ranges[i]=np.sqrt(sonar_points[i].x**2+sonar_points[i].y**2)*2*(1/(1+np.exp(-1*(min(self.hideout))))-0.5)
-        minimum  = np.argmin(sonar_ranges)
-        output   = Point()
+            sonar_ranges[i] = np.sqrt(sonar_points[i].x**2+sonar_points[i].y**2) \
+                * 2*(1/(1+np.exp(-1*(min(self.hideout))))-0.5)
+        minimum = np.argmin(sonar_ranges)
+        output = Point()
         if sonar_ranges[minimum] <= max_distance:
             output.x = sonar_ranges[minimum]*weight[minimum]
             output.y = -sonar_angles[minimum]
         pub.publish(output)
-if __name__== '__main__':
+
+
+if __name__ == '__main__':
     rospy.init_node("obstacle_detection")
     try:
-        node=Sonar()
+        node = Sonar()
     except rospy.ROSInterruptException:
         rospy.loginfo("sonar not working")
